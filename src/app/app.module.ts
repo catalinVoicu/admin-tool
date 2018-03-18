84c36bee1c94580748ee1a57f2beffe70f16d3a0
Catalin Voicu <catalinvoicu91@gmail.com>
2018-03-18 16:55:15 +0200

    Create 3 main components and add bootstrap4.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContentComponent } from './content/content.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    ContentComponent
   ],
   imports: [
+    BsDropdownModule.forRoot(),
     BrowserModule
   ],
   providers: [],
